ffc999807f1ee8937ef6c69bd3aa874aa5f5d22d
underdog1118 <76549705+underdog1118@users.noreply.github.com>
2023-07-26 05:23:05 -0700

    update location

--- auth-14-finished/backend/util/location.js
+++ auth-14-finished/backend/util/location.js
@@ -1,72 +1,72 @@
-// const axios = require('axios');
+const axios = require('axios');
 
-// const HttpError = require('../models/http-error');
+const HttpError = require('../models/http-error');
 
-// const API_KEY = process.env.GOOGLE_API_KEY;
-// // const API_KEY = 'AIzaSyDgLmMpKCzveJf1_yuA0fUzzhy0WRChvZA';
+const API_KEY = process.env.GOOGLE_API_KEY;
+// const API_KEY = 'AIzaSyDgLmMpKCzveJf1_yuA0fUzzhy0WRChvZA';
 
-// async function getCoordsForAddress(address) {
-//   return {
-//     lat: 40.7484474,
-//     lng: -73.9871516
-//   };
-//   // const response = await axios.get(
-//   //   `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
-//   //     address
-//   //   )}&key=${API_KEY}`
-//   // );
-
-//   const data = response.data;
-
-//   if (!data || data.status === 'ZERO_RESULTS') {
-//     const error = new HttpError(
-//       'Could not find location for the specified address.',
-//       422
-//     );
-//     throw error;
-//   }
-
-//   const coordinates = data.results[0].geometry.location;
-
-//   return coordinates;
-// }
-
-// module.exports = getCoordsForAddress;
-
-
-
-
-const axios = require("axios");
-const HttpError = require("../models/http-error");
-const API_KEY = "YOUR_API_KEY_HERE";
- 
 async function getCoordsForAddress(address) {
-  const response = await axios.get(
-    `https://us1.locationiq.com/v1/search.php?key=${pk.b7389606f6a4fbf420faaeec386e1b4d}&q=${encodeURIComponent(
-      address
-    )}&format=json`
-  );
- 
-  const data = response.data[0];
- 
-  console.log(data);
- 
-  if (!data || data.status === "ZERO_RESULTS") {
+  return {
+    lat: 40.7484474,
+    lng: -73.9871516
+  };
+  // const response = await axios.get(
+  //   `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+  //     address
+  //   )}&key=${API_KEY}`
+  // );
+
+  const data = response.data;
+
+  if (!data || data.status === 'ZERO_RESULTS') {
     const error = new HttpError(
-      "Could not find location for the specified address.",
+      'Could not find location for the specified address.',
       422
     );
     throw error;
   }
- 
-  const coorLat = data.lat;
-  const coorLon = data.lon;
-  const coordinates = {
-    lat: coorLat,
-    lng: coorLon
-  };
- 
+
+  const coordinates = data.results[0].geometry.location;
+
   return coordinates;
 }
- 
+
 module.exports = getCoordsForAddress;
+
+
+
+
+// const axios = require("axios");
+// const HttpError = require("../models/http-error");
+// const API_KEY = "YOUR_API_KEY_HERE";
+ 
+// async function getCoordsForAddress(address) {
+//   const response = await axios.get(
+//     `https://us1.locationiq.com/v1/search.php?key=${API_KEY}&q=${encodeURIComponent(
+//       address
+//     )}&format=json`
+//   );
+ 
+//   const data = response.data[0];
+ 
+//   console.log(data);
+ 
+//   if (!data || data.status === "ZERO_RESULTS") {
+//     const error = new HttpError(
+//       "Could not find location for the specified address.",
+//       422
+//     );
+//     throw error;
+//   }
+ 
+//   const coorLat = data.lat;
+//   const coorLon = data.lon;
+//   const coordinates = {
+//     lat: coorLat,
+//     lng: coorLon
+//   };
+ 
+//   return coordinates;
+// }
+ 
+// module.exports = getCoordsForAddress;
